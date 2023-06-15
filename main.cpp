779cddcf1ff640173eb59769d8bbd309a79914a2
Ricard Hw <ric4rd11@gmail.com>
2023-06-14 20:18:28 -0600

    main

--- main.cpp
+++ main.cpp
@@ -23,9 +23,10 @@ int main(int argc, char* argv[]) {
   vector<SimpleShip*> ships;
   vector<Port*> ports;
 
-  int idContainer, idPorts, idShip;
-  idContainer = idPorts = idShip = 0;
+  int idContainer, idPort, idShip;
+  idContainer = idPort = idShip = 0;
 
+  int c, s, p, n;
 
   if (argc != 3) {
     cout << "usage: " << argv[0] << " input_file output_file\n";
@@ -46,13 +47,60 @@ int main(int argc, char* argv[]) {
   puertos
   ship
 */
+
   inputFile>>c>>s>>p>>n;
-  for(){
+
+  for(int i = 0 ; i <= n ; i++){
+
+    int eventType;
+    inputFile>> eventType;
+    int weight = 0;
+    string type = "";
+
+    switch (eventType){
+      case (1): //Create a container
+        inputFile>> idPort;
+        inputFile>> weight;
+        inputFile>> type;
+
+        if (type == "B"){
+          if (weight <= 3500){
+            containers[idContainer] = new LightContainer(idContainer, weight);
+
+          } else {
+            containers[idContainer] = new HeavyContainer(idContainer, weight);
+          }
+        } else if (type == "R"){
+          containers[idContainer] = new RefrigeratedContainer(idContainer, weight);
+        } else if (type == "L"){
+          containers[idContainer] = new LiquidContainer(idContainer, weight);
+        }
+        *ports[idPort].add(containers[idContainer]);
+        idContainer++;
+
+      break;
+      case (2): //Create a ship
+
+      break;
+      case (3): //Create a port
+
+      break;
+      case (4): //Load a container to a ship
+
+      break;
+      case (5): //Unload a container from a ship
+
+      break;
+      case (6): //Sail a ship to a port
+
+      break;
+      case (7): //Refuel a ship
+
+      break;
+    }
 
   }
-  inputFile>>tipoEvento;
 
-  inputFile>>x>>y;
   ports[idPorts] = new Port(idPorts, x, y);
   idPorts++;
 
